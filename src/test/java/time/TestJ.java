1f8d9ece93d2d627df4456ea804f100c6f370da5
malexj <alexmaximovj@gmail.com>
2017-07-13 20:14:02 +0300

    fix

--- src/test/java/time/TestJ.java
+++ src/test/java/time/TestJ.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
@@ -27,9 +28,10 @@ public class TestJ {
 		String requestFromClient = "Europe/Kiev";
 
 
-		Instant instant = Instant.ofEpochMilli(1499264104780L);
+		Instant instant = Instant.ofEpochMilli(18000000);
 		instant.atZone(ZoneId.of(requestFromClient));
 		System.out.println(instant);
+		System.out.println(instant.toEpochMilli());
 
 
 		OffsetDateTime offsetDateTime = instant.atOffset(ZoneOffset.UTC);
@@ -53,49 +55,11 @@ public class TestJ {
 	@Test
 	public void test() {
 
-
-//		String str = "1970-01-01 09:00";
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-//		LocalDateTime localtDateAndTime = LocalDateTime.parse(str, formatter);
-
-		ZonedDateTime dateAndTimeInSydney = ZonedDateTime.of(LocalDate.of(1970, 1, 1), LocalTime.of(9, 0), ZoneId.of("Europe/Kiev"));
-		System.out.println("Current date and time in XXX : " + dateAndTimeInSydney.toLocalTime());
-
-		System.out.println(dateAndTimeInSydney.toInstant().toEpochMilli());
-
-		ZonedDateTime utcDate = dateAndTimeInSydney.withZoneSameInstant(ZoneId.of("Europe/Kiev"));
-		System.out.println("Current date and time in Europe/Kiev : " + utcDate.toLocalTime());
-
-
 		// ANDROID
 		Calendar cal = Calendar.getInstance();
-		cal.setTimeInMillis(21600000);
-//		cal.setTimeZone(TimeZone.getTimeZone("CDT"));
+		cal.setTimeInMillis(1499335228235L);
 
-		System.out.println("TTT " + cal.getTime());
-//
-//		String dateMillis = createDateMillis("09:00", "Europe/Kiev");
-//
-//		LocalTime localTime = LocalTime.of(9, 0);
-
-////		System.out.println("ldt "+ ldt);
-//
-//
-//		LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.EPOCH, ZoneId.of("Europe/Kiev"));
-//
-//
-//		System.out.println("localDateTime "+localDateTime);
-
-
-//		Instant instant = Instant.EPOCH;
-//
-//
-//
-//		ZoneId zoneId = ZoneId.of( "Europe/Kiev");
-//		ZonedDateTime zdt = ZonedDateTime.ofInstant( instant , zoneId );
-//
-//		System.out.println(zdt.toOffsetDateTime());
-//		System.out.println(zdt.toEpochSecond());
+		System.out.println("Time: " + cal.getTime());
 
 	}
 
